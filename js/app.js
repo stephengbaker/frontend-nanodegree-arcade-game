6655e749c9a96db0e521d68a1fb0fc3fe137f838
stephengbaker <stephengbaker@yahoo.com>
2015-04-10 13:34:22 -0500

    Got bugs moving
    Got some bugs moving and the character to show. Still need to get input
    method on character.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(yval, xval) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = xval;
+    this.y = yval;
 }
 
 // Update the enemy's position, required method for game
@@ -14,6 +16,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x <600) {
+        this.x = this.x + (dt * 100);
+    }
+    else {
+        this.x = -200;
+    }
 }
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +32,32 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 400;
+}
 
+Player.prototype.update = function(dt) {
+    // this.x = this.x + this.changeX;
+    // this.y = this.y + this.changeY;
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function() {
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [new Enemy(150, -200), new Enemy(100, -300)];
+
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
